2f65a9ed5c395ba6d9798f085fb082c3f90a9a22
Simone Fregonese <simonefregonese2@hotmail.it>
2017-06-20 15:00:36 +0200

    Modificato Startup e appsettings

--- src/RilevazioneAccessi/Startup.cs
+++ src/RilevazioneAccessi/Startup.cs
@@ -28,6 +28,11 @@ namespace RilevazioneAccessi
         public void ConfigureServices(IServiceCollection services)
         {
             // Add framework services.
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+
             services.AddMvc();
         }
 
